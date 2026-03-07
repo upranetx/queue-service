f348b4d3761ed046e0054f719dd41dd92c84cb8b
upranetx <bayupranata@libertymail.co>
2026-03-08 01:07:39 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,8 +1,2 @@
 // push 03/08/2026 00:50:09
-export default {
-  async fetch(){
-    return new Response("queue-service", {
-      headers: {"content-type":"text/plain"}
-    })
-  }
-}
+export default{async fetch(){return new Response("name_repos",{headers:{"content-type":"text/plain"}})}}
